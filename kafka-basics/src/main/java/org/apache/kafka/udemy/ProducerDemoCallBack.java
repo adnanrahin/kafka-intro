22613cef5f69b64597804ce7a876ecb291cdcff7
adnanrahin <adnan.rahin13@gmail.com>
2023-06-28 22:34:14 -0400

    callback added for error review

--- kafka-basics/src/main/java/org/apache/kafka/udemy/ProducerDemoCallBack.java
+++ kafka-basics/src/main/java/org/apache/kafka/udemy/ProducerDemoCallBack.java
@@ -1,7 +1,9 @@
 package org.apache.kafka.udemy;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,10 +24,28 @@ public class ProducerDemoCallBack {
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        ProducerRecord<String, String> producerRecord =
-                new ProducerRecord<>("demo_java", "hello-world");
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> producerRecord =
+                    new ProducerRecord<>("demo_java", "hello-world" + i);
 
-        producer.send(producerRecord);
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        log.info("Received New Metadata \n" +
+                                "Topic:" + recordMetadata.topic() + " \n" +
+                                "Partition:" + recordMetadata.partition() + " \n" +
+                                "Offset:" + recordMetadata.offset() + " \n" +
+                                "Time Stamp:" + recordMetadata.timestamp() + " \n");
+                    } else {
+                        log.error("Error While Producing");
+                    }
+
+                }
+            });
+
+        }
+
 
         producer.flush();
 
