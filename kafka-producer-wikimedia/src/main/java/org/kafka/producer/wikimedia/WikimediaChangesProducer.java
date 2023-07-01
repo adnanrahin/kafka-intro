c10d9682fabc2a981ad842ddae83187f0ba60e1c
adnanrahin <adnan.rahin13@gmail.com>
2023-07-01 03:51:01 -0400

    update

--- kafka-producer-wikimedia/src/main/java/org/kafka/producer/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/org/kafka/producer/wikimedia/WikimediaChangesProducer.java
@@ -22,6 +22,12 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
