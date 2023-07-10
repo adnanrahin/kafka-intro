3bfe7817c5acc0fac534b6f791030347d3bf3029
adnanrahin <adnan.rahin13@gmail.com>
2023-07-10 02:53:47 -0400

    Run Method Created

---
+++ effective-kafka/src/main/java/org/effective/kafka/sender/RunRandomEventProducer.java
@@ -0,0 +1,25 @@
+package org.effective.kafka.sender;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import java.util.Map;
+
+public class RunRandomEventProducer {
+
+    public static void main(String[] args)
+            throws InterruptedException, EventSender.SendException {
+        final Map<String, Object> config =
+                Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                        "localhost:9092",
+                        ProducerConfig.CLIENT_ID_CONFIG,
+                        "customer-producer-sample");
+        final var topic = "customer.test";
+        try (var sender = new DirectSender(config, topic)) {
+            final var businessLogic = new ProducerBusinessLogic(sender);
+            while (true) {
+                businessLogic.generateRandomEvents();
+                Thread.sleep(500);
+            }
+        }
+    }
+}
