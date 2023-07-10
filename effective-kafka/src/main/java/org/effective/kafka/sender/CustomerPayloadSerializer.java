07fdb84f175af9028d810e1b49a8c1218a3fbfda
adnanrahin <adnan.rahin13@gmail.com>
2023-07-10 02:45:20 -0400

    Serilalizer added

---
+++ effective-kafka/src/main/java/org/effective/kafka/sender/CustomerPayloadSerializer.java
@@ -0,0 +1,28 @@
+package org.effective.kafka.sender;
+
+import org.apache.kafka.common.serialization.*;
+
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+import org.effective.kafka.event.CustomerPayload;
+
+public final class CustomerPayloadSerializer implements Serializer<CustomerPayload> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final class MarshallingException extends RuntimeException {
+        private static final long serialVersionUID = 1L;
+
+        private MarshallingException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+    @Override
+    public byte[] serialize(String topic, CustomerPayload data) {
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (JsonProcessingException e) {
+            throw new MarshallingException(e);
+        }
+    }
+}
