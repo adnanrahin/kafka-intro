157de00f5002f4e153e79a4b892949f9e8583afb
adnanrahin <adnan.rahin13@gmail.com>
2023-07-16 23:23:14 -0400

    Config class property updated

--- effective-kafka/src/main/java/org/effective/kafka/frameworks/AbstractClientConfig.java
+++ effective-kafka/src/main/java/org/effective/kafka/frameworks/AbstractClientConfig.java
@@ -1,4 +1,24 @@
 package org.effective.kafka.frameworks;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public abstract class AbstractClientConfig<C extends AbstractClientConfig<?>> {
+
+    private final Map<String, Object> customEntries = new HashMap<>();
+
+    @SuppressWarnings("unchecked")
+    public final C withCustomEntry(String propertyName, Object value) {
+        Objects.requireNonNull(propertyName, "Property name cannot be null");
+        customEntries.put(propertyName, value);
+        return (C) this;
+    }
+
+    public final Map<String, Object> mapify() {
+        final Map<String, Object> stagingConfig = new HashMap<>();
+
+        return stagingConfig;
+    }
+
 }
