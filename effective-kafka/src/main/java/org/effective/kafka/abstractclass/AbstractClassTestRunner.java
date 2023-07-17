3e2f4782fc8ed0f65296f9db380051556b1bd77b
adnanrahin <adnan.rahin13@gmail.com>
2023-07-16 22:54:08 -0400

    update user api url

--- effective-kafka/src/main/java/org/effective/kafka/abstractclass/AbstractClassTestRunner.java
+++ effective-kafka/src/main/java/org/effective/kafka/abstractclass/AbstractClassTestRunner.java
@@ -8,20 +8,12 @@ public class AbstractClassTestRunner {
         int readTimeout = 10000;
         String apiKey = "your-api-key";
 
-        // Create a UserClient instance with initial configurations
         UserClient userClient = new UserClient(userApiUrl, connectionTimeout, readTimeout, apiKey);
-
-        // Perform actions with the UserClient, such as getting user data, creating users, etc.
-        // ...
-
-        // Update the API URL for the UserClient
         String updatedApiUrl = "https://api.example.com/v2/users";
         UserClient updatedClient = userClient.withApiUrl(updatedApiUrl);
 
         System.out.println(updatedClient);
 
-        // Now, use the updatedClient for further operations with the new API URL
-        // ...
     }
 
 }
